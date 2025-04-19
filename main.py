064bbc3b97a79814bdde17ca42f91a38bca05ec4
yogesh <ypc@gmail.com>
2025-04-19 14:43:00 +0530

    changes in nain.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 print("Hello Yogesh in JKS")
 print("JAY SHREE RAM...")
-<<<<<<< HEAD
-print('best luck.brooooo')
-=======
-print('best luck..')
->>>>>>> b274d3d8afbc09a73498cee9445b486c0ae08d4c
+print('AUtomation SCEESSS...')
